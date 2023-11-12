68c7626a81e91059b6ee8e7cf64adb2922ad4afc
pauli090 <paulinkaa090@gmail.com>
2023-11-12 20:35:14 +0000

    added text on save buttons in html file, cleaner js file, added comments

--- script.js
+++ script.js
@@ -2,14 +2,11 @@
 var currentdayEl = dayjs().format('dddd DD MMMM');
 $("#currentDay").text(currentdayEl);
 
-var hourEl = document.querySelector(".hour");
-
 // Present timeblocks for standard business hours when the user scrolls down.
-
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-//for every hour in the rows change description column to different colour acording current time
+var hourEl = document.querySelector(".hour");
 var now = dayjs().hour();
 
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 $(".time-block").each(function() {
     var timeBlock = parseInt($(this).attr("id").split("-")[1]);
     if (timeBlock < now) {
@@ -24,24 +21,15 @@ $(".time-block").each(function() {
     }
 })
 
+// Allow a user to enter an event when they click a timeblock
+// Save the event in local storage when the save button is clicked in that timeblock.
 $(".saveBtn").on("click", function() {
     var time = $(this).parent().attr("id");
     var value = $(this).siblings(".description").val();
     localStorage.setItem(time, value);
 })
 
+// Persist events between refreshes of a page
 for(var i = 9; i < 18; i++) {
     $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i))
 }
-
-// function colorCodeblock() {
-    
-//     $(".description").addClass("present");
-        
-//     }
-
-// Allow a user to enter an event when they click a timeblock
-
-// Save the event in local storage when the save button is clicked in that timeblock.
-
-// Persist events between refreshes of a page
